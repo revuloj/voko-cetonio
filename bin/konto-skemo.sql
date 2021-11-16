188460df70c4eaecff79645e6efdae72138d90a6
Wolfram Diestel <diestel@steloj.de>
2021-11-16 20:00:21 +0100

    aktualigita konto-skemo

--- bin/konto-skemo.sql
+++ bin/konto-skemo.sql
@@ -1,8 +1,10 @@
 CREATE TABLE redaktanto (
   red_id VARCHAR(20) PRIMARY KEY NOT NULL,
   nomo VARCHAR(100) NOT NULL,
-  openid TEXT,
-  subid TEXT
+  subid_gg TEXT,
+  subid_yh TEXT,
+  subid_fb TEXT,
+  subid_gh TEXT
 );
 
 CREATE TABLE retposhto (
@@ -16,12 +18,15 @@ CREATE VIEW _redaktanto_poshto_unu AS
 SELECT
   r.red_id as red_id,
   r.nomo as nomo,
-  r.openid as openid,
-  r.subid as subid,
+  r.subid_gg as subid_gg,
+  r.subid_yh as subid_yh,
+  r.subid_fb as subid_fb,
+  r.subid_gh as subid_gh,
   p.retposhto as retposhto
 FROM redaktanto r, retposhto p
 WHERE r.red_id=p.red_id
   AND p.numero = 1;
+
 
 
 
