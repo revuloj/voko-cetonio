a3df5ae5bd627ca11835ee0efead94538d086c40
Wolfram Diestel <diestel@steloj.de>
2021-11-21 10:07:25 +0100

    nova surmetita pozicifenestreto...

--- bin/deplojo.sh
+++ bin/deplojo.sh
@@ -3,6 +3,7 @@
 target="${1:-docker}"
 
 HTML=pro/web/redaktilo.html
+CSS=pro/web/static/redaktilo.css
 
 case $target in
 
@@ -11,5 +12,7 @@ docker)
     todir=/home/cetonio/pro
     echo "${HTML} -> ${cetonio_id}:${todir}/web"
     docker cp ${HTML} ${cetonio_id}:${todir}/web
-
+    echo "${CSS} -> ${cetonio_id}:${todir}/web/static"
+    docker cp ${CSS} ${cetonio_id}:${todir}/web/static
+    ;;
 esac
